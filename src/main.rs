fc704500cecdce5a765e37cb7896f97aed654def
itsgerliz <gerworks.hs@gmail.com>
2025-01-15 09:47:46 +0100

    ft: clap argument struct and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use clap::Parser;
 use log::{error, warn, info, debug, trace};
 
-fn main() {
-    env_logger::init();
+#[derive(Parser)]
+#[command(version, about)]
+struct CliArgs {
+    #[arg(help = "IP address to connect to", required = true)]
+    target: String,
+    #[arg(help = "Port to connect to", required = true)]
+    target_port: u16
+}
 
-    
+fn main() {
+    let args = CliArgs::parse();
+    env_logger::init();
 }
