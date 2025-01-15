c6b166917e82004aa8f0cae9b71e4ed6fa69d386
itsgerliz <gerworks.hs@gmail.com>
2025-01-15 10:02:33 +0100

    fix: forgot server argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
-use log::{error, warn, info, debug, trace};
+use log::{error, warn, info, debug};
 
 #[derive(Parser)]
 #[command(version, about)]
 struct CliArgs {
+    #[arg(help = "Run as server?", required = false)]
+    server: bool,
     #[arg(help = "IP address to connect to", required = true)]
     target: String,
     #[arg(help = "Port to connect to", required = true)]
@@ -12,5 +14,16 @@ struct CliArgs {
 
 fn main() {
     let args = CliArgs::parse();
+    
     env_logger::init();
+    debug!("Initiated logger!");
+
+    let target_addr = todo!();
+
+    if args.server == true {
+        todo!()
+    } else {
+        todo!()
+    }
+
 }
