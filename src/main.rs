434903f49b29fca2babceeb6439681bcdcf89531
itsgerliz <gerworks.hs@gmail.com>
2025-01-16 13:08:42 +0100

    feat: set default logging level to info and set header constants

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,24 @@ use log::{info, debug};
 mod client;
 mod server;
 
+const HEADER_MAGIC: &[u8] = "udpChat".as_bytes();
+const UDPCHAT_VER: &[u8] = "v1.0.0".as_bytes();
+
 #[derive(Parser)]
 #[command(version, about)]
 struct CliArgs {
-    #[arg(help = "Run as server?", required = false)]
+    #[arg(help = "Run as server?", short = 'l', long = "listen", required = false)]
     server: bool,
-    #[arg(help = "IP address to connect to", required = true)]
+    #[arg(help = "Where should we listen?", required = true)]
     target: String,
-    #[arg(help = "Port to connect to", required = true)]
+    #[arg(help = "Port to listen on", required = true)]
     target_port: u16
 }
 
 fn main() {
     let args = CliArgs::parse();
     
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     debug!("Initiated logger!");
 
     let target_addr = (args.target.as_str(), args.target_port);
