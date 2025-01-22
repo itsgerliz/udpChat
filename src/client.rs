5f4889c4d3446dcde43c0710322bce72a7e80428
itsgerliz <gerworks.hs@gmail.com>
2025-01-22 12:45:37 +0100

    ft (client): move header creation to main client function

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
 use std::net::UdpSocket;
 use std::process::exit;
 use log::{error, info, debug};
-use crate::{HEADER_SIZE, HEADER_MAGIC, HEADER_VERSION, HEADER_MSGT_LOGIN};
+use crate::{HEADER_SIZE, HEADER_MAGIC, HEADER_VERSION, HEADER_MSGT_LOGIN, HEADER_MSGT_LOGOUT};
 
 pub(crate) fn init(target: &(&str, u16)) {
 	let socket = match UdpSocket::bind(("127.0.0.1", 0)) {
@@ -15,24 +15,30 @@ pub(crate) fn init(target: &(&str, u16)) {
 		}
 	};
 
-	match send_login(&socket, target) {
-		true => info!("OK"),
-		false => error!("Not ok")
-	}
-}
-
-fn send_login(socket: &UdpSocket, endpoint: &(&str, u16)) -> bool {
-	// Compound header to send
+	// Create the header here to avoid recreating it on each function that needs it
 	let mut header: [u8; HEADER_SIZE as usize] = [0; HEADER_SIZE as usize];
+	// Header magic and version are the same for the entire runtime
 	header[..HEADER_MAGIC.len()].copy_from_slice(HEADER_MAGIC);
 	header[HEADER_MAGIC.len()] = HEADER_VERSION;
-	header[HEADER_MAGIC.len() + 1] = HEADER_MSGT_LOGIN;
+	// Message type is initialized to 0 but must be filled by the function
+	header[HEADER_MAGIC.len() + 1] = 0;
+	// The message type will be filled by the function with the following:
+	// header[HEADER_MAGIC.len() + 1] = <messagetype>;
+	// The function must reset this to zero when done
 
-	match socket.send_to(&header, *endpoint) {
+	send_login(&socket, target, &mut header);
+}
+
+fn send_login(socket: &UdpSocket, endpoint: &(&str, u16), header: &mut [u8; HEADER_SIZE as usize]) {
+	// Add message type
+	(*header)[HEADER_MAGIC.len() + 1] = HEADER_MSGT_LOGIN;
+
+	match socket.send_to(header, *endpoint) {
 		Ok(bsent) => {
 			assert_eq!(bsent, header.len());
 			debug!("Sent header to {}:{}", (*endpoint).0, (*endpoint).1);
-			true
+			// Reset message type field
+			(*header)[HEADER_MAGIC.len() + 1] = 0
 		}
 		Err(err) => {
 			error!("Could not send header to specified address, OS sent: {}", err);
@@ -40,3 +46,9 @@ fn send_login(socket: &UdpSocket, endpoint: &(&str, u16)) -> bool {
 		}
 	}
 }
+
+/*
+fn await_response(socket: &UdpSocket, endpoint: &(&str, u16)) {
+
+}
+*/
